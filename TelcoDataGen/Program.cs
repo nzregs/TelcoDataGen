dac9d01fa78757aa79b8e743df49585c304fcb82
Regan Murphy <rmurphy@microsoft.com>
2018-02-06 22:52:41 +1300

    initial vs commit

---
+++ TelcoDataGen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Collections.Generic;
+using Microsoft.ServiceBus.Messaging;
+
+namespace TelcoDataGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
